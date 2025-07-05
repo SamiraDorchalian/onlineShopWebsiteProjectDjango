ba06f6cbc237d3332c5ea13006553b79e16f780b
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-05 16:51:20 +0330

    add envirement variables for django and secret key

--- onlineShopWebsiteProjectDjango-with-db.sqlite3/config/settings.py
+++ onlineShopWebsiteProjectDjango-with-db.sqlite3/config/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+# for environment variables
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s6n#u4s2hn(w)jmt3hd7)t8mpya^wn6^_(m-t@di=345n4@xg@'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
 
 # Application definition
