b85344942ec61cae854040c19c463849575ba250
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-07 18:13:19 +0330

    add tranlation & messages framework & rosetta

--- onlineShopWebsiteProjectDjango-with-db.sqlite3/products/admin.py
+++ onlineShopWebsiteProjectDjango-with-db.sqlite3/products/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 
 from .models import Product, Comment
 
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active']
+    extra = 1
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', 'datetime_created', 'datetime_modified', ]
+
+    inlines = [
+        CommentsInline,
+    ]
 
 
 @admin.register(Comment)
--- onlineShopWebsiteProjectDjango-with-db.sqlite3/products/models.py
+++ onlineShopWebsiteProjectDjango-with-db.sqlite3/products/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.PositiveIntegerField(default=0)
+    title = models.CharField(max_length=100, verbose_name=_('Title'))
+    description = models.TextField(verbose_name=_('Description'))
+    price = models.PositiveIntegerField(default=0, verbose_name=_('Price'))
     active = models.BooleanField(default=True)
 
     datetime_created = models.DateTimeField(auto_now_add=True)
@@ -16,25 +17,36 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("product_detail", args=[self.pk])
-    
+
+
+class ActiveCommentsManger(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentsManger, self).get_queryset().filter(active=True)
+
 
 class Comment(models.Model):
     PRODUCT_STARS = [
-        ('1', 'Very Bad'),
-        ('2', 'Bad'),
-        ('3', 'Normal'),
-        ('4', 'Good'),
-        ('5', 'Perfect'),
+        ('1', '⭐'),
+        ('2', '⭐⭐'),
+        ('3', '⭐⭐⭐'),
+        ('4', '⭐⭐⭐⭐'),
+        ('5', '⭐⭐⭐⭐⭐'),
     ]
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', )
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments', )
-    body = models.TextField()
-    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments', verbose_name=_('Comment author'))
+    body = models.TextField(verbose_name=_('Text Comment'))
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_('What is your score?'))
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
     active = models.BooleanField(default=True)
 
+    # Manager
+    objects = models.Manager()
+    active_comments_manager = ActiveCommentsManger()
 
+    def get_absolute_url(self):
+        return reverse("product_detail", args=[self.product.id])
+    
