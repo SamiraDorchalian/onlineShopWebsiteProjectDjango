f48cb30dc2ba16047ac6789148140fc87d95f597
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-07 14:33:58 +0330

    add CommentModel

--- onlineShopWebsiteProjectDjango-with-db.sqlite3/products/admin.py
+++ onlineShopWebsiteProjectDjango-with-db.sqlite3/products/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', 'datetime_created', 'datetime_modified', ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['product', 'author', 'body', 'stars', 'datetime_created', 'datetime_modified', 'active']
