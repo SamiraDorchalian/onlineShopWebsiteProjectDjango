733d35cd04bca7d872e0b1ec52d1684a32561cbe
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-08 20:02:53 +0330

    add DjangoJalaliDate & make PersianTranslateNumber

--- onlineShopWebsiteProjectDjango-with-db.sqlite3/config/settings.py
+++ onlineShopWebsiteProjectDjango-with-db.sqlite3/config/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # third party
+    # Django Jalali Date   
+    'jalali_date',
     # Crispy
     'rosetta',
     "crispy_forms",
@@ -61,6 +63,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'products.apps.ProductsConfig',
     'cart.apps.CartConfig',
+    'persian_translate.apps.PersianTranslateConfig',
 ]
 
 MIDDLEWARE = [
--- onlineShopWebsiteProjectDjango-with-db.sqlite3/products/admin.py
+++ onlineShopWebsiteProjectDjango-with-db.sqlite3/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from jalali_date.admin import ModelAdminJalaliMixin
 from .models import Product, Comment
 
 
@@ -9,7 +9,7 @@ class CommentsInline(admin.TabularInline):
     extra = 1
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
     list_display = ['title', 'price', 'active', 'datetime_created', 'datetime_modified', ]
 
     inlines = [
