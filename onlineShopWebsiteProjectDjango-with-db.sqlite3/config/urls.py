2e2b18ee8111ef4094398c649cb965630206d769
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-08 18:40:47 +0330

    add product image field

--- onlineShopWebsiteProjectDjango-with-db.sqlite3/config/urls.py
+++ onlineShopWebsiteProjectDjango-with-db.sqlite3/config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
@@ -26,4 +29,6 @@ urlpatterns = [
 
     # Rosetta
     path('rosetta/', include('rosetta.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
