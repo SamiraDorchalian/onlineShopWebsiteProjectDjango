e6104acfde97189688d8efab5dbad088a485e5a7
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-09 12:46:59 +0330

    add Order & OrderItem Models

--- onlineShopWebsiteProjectDjango-with-db.sqlite3/config/settings.py
+++ onlineShopWebsiteProjectDjango-with-db.sqlite3/config/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'cart.apps.CartConfig',
     'persian_translate.apps.PersianTranslateConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
