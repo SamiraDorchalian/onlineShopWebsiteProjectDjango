6313c1f74f6cb0b46109970d4f494432e0e2bc7e
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-09 16:54:19 +0330

    implement order process

--- onlineShopWebsiteProjectDjango-with-db.sqlite3/orders/models.py
+++ onlineShopWebsiteProjectDjango-with-db.sqlite3/orders/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.conf import settings
 from products.models import Product
+from django.utils.translation import gettext as _
 
 
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    is_paid = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_('User'))
+    is_paid = models.BooleanField(default=False, verbose_name=_('IS Paid?'))
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
-    address = models.CharField(max_length=700)
+    first_name = models.CharField(max_length=100, verbose_name=_('First Name'))
+    last_name = models.CharField(max_length=100, verbose_name=_('Last Name'))
+    phone_number = models.CharField(max_length=15, verbose_name=_('Phone Number'))
+    address = models.CharField(max_length=700, verbose_name=_('Address'))
 
-    order_notes = models.CharField(max_length=700, blank=True)
+    order_notes = models.CharField(max_length=700, blank=True, verbose_name=_('Order Notes'))
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
@@ -22,10 +23,10 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
-    quantity = models.PositiveIntegerField(default=1)
-    price = models.PositiveIntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items', verbose_name=_('Order'))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items', verbose_name=_('Product'))
+    quantity = models.PositiveIntegerField(default=1, verbose_name=_('Quantity'))
+    price = models.PositiveIntegerField(verbose_name=_('Price'))
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
--- onlineShopWebsiteProjectDjango-with-db.sqlite3/products/models.py
+++ onlineShopWebsiteProjectDjango-with-db.sqlite3/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 
 from ckeditor.fields import RichTextField
 
