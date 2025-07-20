ff005ee8cb8e188d6465f1b3df674a437913750c
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-20 18:21:47 +0330

    add ProductDetailView

--- onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/products/urls.py
+++ onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
     
 ]
