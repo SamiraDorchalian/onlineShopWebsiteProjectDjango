84762291117ebfdc38056d574116a1feb07cf2b8
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-20 18:09:10 +0330

    make ProductListView

--- onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/config/urls.py
+++ onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     # path('accounts/', include('django.contrib.auth.urls')),
     # path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')), # Allauth
+    path('products/', include('products.urls')),
 ]
