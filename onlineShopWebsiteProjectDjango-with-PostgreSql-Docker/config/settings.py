b47426d23bfd9a73fdeac8c1df90697fbb295c96
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-20 17:19:17 +0330

    change some accounts templates

--- onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/config/settings.py
+++ onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/config/settings.py
@@ -100,7 +100,13 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "samiradorchalain1998@gmail.com"
+EMAIL_HOST_PASSWORD = "1998samira"
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
