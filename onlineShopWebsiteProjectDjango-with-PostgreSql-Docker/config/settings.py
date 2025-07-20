9669b022b7fb30cad4aa1c3c57a5e198eeed3619
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-20 16:54:30 +0330

    add environment variables for debug and secret key

--- onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/config/settings.py
+++ onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/config/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+# for environment variables
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_^sf*f16a==q9_m5&yt125#_c^pz!a4p6w2xl!=+b@ko9*sx5z'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
 
 
 # Application definition
