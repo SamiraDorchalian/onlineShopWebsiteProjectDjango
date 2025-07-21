e6bf0176bf8ae6c2057b185eac0185aef4782473
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-21 12:28:49 +0330

    add CommentModel

--- onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/products/admin.py
+++ onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/products/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', 'datetime_created' ,'datetime_modified']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['product', 'author', 'text', 'stars' ,'active', 'datetime_created' ,'datetime_modified']
