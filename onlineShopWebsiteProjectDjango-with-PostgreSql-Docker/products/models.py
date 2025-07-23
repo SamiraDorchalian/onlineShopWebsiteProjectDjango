1e796feb2af74446385636f10fbbef583c1e607b
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-23 15:38:58 +0330

    add translation to productsApp

--- onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/products/models.py
+++ onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/products/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.PositiveIntegerField(default=0)
-    active = models.BooleanField(default=True)
+    title = models.CharField(max_length=100, verbose_name=_('Title'))
+    description = models.TextField(verbose_name=_('Description'))
+    price = models.PositiveIntegerField(default=0, verbose_name=_('Price'))
+    active = models.BooleanField(default=True, verbose_name=_('Active'))
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
@@ -33,15 +34,15 @@ class Comment(models.Model):
         ('6', '⭐⭐⭐⭐⭐⭐'),
     ]
 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', )
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments', verbose_name='Comment Author' )
-    body = models.TextField(verbose_name='Comment Text')
-    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name='What is your score?')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', verbose_name=_('Product'), )
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments', verbose_name=_('Comment Author'), )
+    body = models.TextField(verbose_name=_('Comment Text'))
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_('What is your score?'))
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default=True, verbose_name=_('Active'))
 
     # Manager
     objects = models.Manager()
