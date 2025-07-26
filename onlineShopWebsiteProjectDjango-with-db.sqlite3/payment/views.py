9021f2f767cd6d6018ecc0236ad95c3abb599a23
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-26 18:11:59 +0330

    Update .gitignore File & update requirements.txt File

--- onlineShopWebsiteProjectDjango-with-db.sqlite3/payment/views.py
+++ onlineShopWebsiteProjectDjango-with-db.sqlite3/payment/views.py
@@ -1,100 +1,13 @@
-import requests 
+import requests
 import json
 
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.conf import settings
 from django.http import HttpResponse
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 from orders.models import Order
-
-# def payment_process(request):
-#     # Get order id from session
-#     order_id = request.session.get('order_id')
-#     # Get the order object
-#     order = get_object_or_404(Order, id=order_id)
-
-#     toman_total_price = order.get_total_price()
-#     rial_total_price = toman_total_price * 10
-
-#     zarinpal_request_url = 'https://api.zarinpal.com/pg/v4/payment/request.json'
-#     # zarinpal_request_url = 'https://sandbox.zarinpal.com/pg/v4/payment/request.json'
-
-#     request_header = {
-#         "accept": "application/json",
-#         "content-type": "application/json",
-#     }
-
-#     request_data = {
-#         'merchant_id': settings.ZARINPAL_MERCHANT_ID,
-#         'amount': rial_total_price,
-#         'description': f'#{order.id}: {order.user.first_name} {order.user.last_name}',
-#         'callback_url': request.build_absolute_uri(reverse('payment:payment_callback')),
-#     }
-
-#     res = requests.post(url=zarinpal_request_url, data=json.dumps(request_data), headers=request_header)
-    
-#     data = res.json()['data']
-#     authority = data['authority']
-#     order.zarinpal_authority = authority
-#     order.save()
-
-#     if 'errors' not in data or len(data['errors']) == 0:
-#         return redirect('https://www.zarinpal.com/pg/StartPay/{authority}'.format(authority=authority))
-#         # return redirect('https://sandbox.zarinpal.com/pg/StartPay/{authority}'.format(authority=authority))
-#     else:
-#         return HttpResponse('Error from zarinpal')
-
-
-# def payment_callback(request):
-#     payment_authority = request.GET.get('Authority')
-#     payment_status = request.GET.get('Status')
-
-#     order = get_object_or_404(Order, zarinpal_authority=payment_authority)
-#     toman_total_price = order.get_total_price()
-#     rial_total_price = toman_total_price * 10
-
-#     if payment_status == 'OK':
-#         request_header = {
-#             "accept": "application/json",
-#             "content-type": "application/json",
-#         }
-
-#         request_data = {
-#             'merchant_id': settings.ZARINPAL_MERCHANT_ID,
-#             'amount': rial_total_price,
-#             'authority': payment_authority,
-#         }
-
-#         res = request.post(
-#             url='https://api.zarinpal.com/pg/v4/payment/verify.json',
-#             # url='https://sandbox.zarinpal.com/pg/v4/payment/verify.json',
-#             data=json.dumps(request_data),
-#             headers=request_header,
-#         )
-        
-#         if 'data' in res.json() and ('errors' not in res.json()['data'] or len(res.json()['data']['errors']) == 0):
-#             data = res.json()['data']
-#             payment_code = data['code']
-
-#             if payment_code == 100:
-#                 order.is_paid = True
-#                 order.ref_id = data['ref_id']
-#                 order.zarinpal_data = data
-#                 order.save()
-
-#                 return HttpResponse('پرداخت شما با موفقیت انجام شد.')
-
-#             elif payment_code == 101:
-#                 return HttpResponse('پرداخت شما با موفقیت ثبت شد. البته این تراکنش قبلا ثبت شده است.')
-
-#             else:
-#                 error_code = res.json()['errors']['code']
-#                 error_message = res.json()['errors']['message']
-#                 return HttpResponse(f'تراکنش نا موفق بود{error_code} {error_message}.')
-
-#     else:
-#         return HttpResponse(f'تراکنش نا موفق بود.')
-
 
 def payment_process(request):
     # Get order id from session
@@ -105,7 +18,7 @@ def payment_process(request):
     toman_total_price = order.get_total_price()
     rial_total_price = toman_total_price * 10
 
-    # zarinpal_request_url = 'https://api.zarinpal.com/pg/v4/payment/request.json'
+    # zarinpal_request_url = 'https://payment.zarinpal.com/pg/v4/payment/request.json'
     zarinpal_request_url = 'https://sandbox.zarinpal.com/pg/v4/payment/request.json'
 
     request_header = {
@@ -114,27 +27,25 @@ def payment_process(request):
     }
 
     request_data = {
-        'merchant_id': "f588be9a-5417-4c12-991e-b3aa81640516",
+        'merchant_id': settings.ZARINPAL_MERCHANT_ID,
         'amount': rial_total_price,
         'description': f'#{order.id}: {order.user.first_name} {order.user.last_name}',
-        'callback_url': request.build_absolute_uri(reverse('payment:payment_callback')),
+        # 'callback_url': 'http://127.0.0.1:8000' + reverse('payment:payment_callback'),
+        'callback_url':request.build_absolute_uri(reverse('payment:payment_callback')),
     }
 
     res = requests.post(url=zarinpal_request_url, data=json.dumps(request_data), headers=request_header)
     
-    print(res)
     data = res.json()['data']
-    print(data)
     authority = data['authority']
     order.zarinpal_authority = authority
     order.save()
 
     if 'errors' not in data or len(data['errors']) == 0:
-        # return redirect('https://www.zarinpal.com/pg/StartPay/{authority}'.format(authority=authority))
+        # return redirect('https://payment.zarinpal.com/pg/StartPay/{authority}'.format(authority=authority))
         return redirect('https://sandbox.zarinpal.com/pg/StartPay/{authority}'.format(authority=authority))
     else:
         return HttpResponse('Error from zarinpal')
-
 
 
 def payment_callback(request):
@@ -152,21 +63,20 @@ def payment_callback(request):
         }
 
         request_data = {
-            'merchant_id': "f588be9a-5417-4c12-991e-b3aa81640516",
+            'merchant_id': settings.ZARINPAL_MERCHANT_ID,
             'amount': rial_total_price,
-            'authority': payment_authority,
+            'authority': payment_authority
         }
 
         res = requests.post(
-            # url='https://api.zarinpal.com/pg/v4/payment/verify.json',
+            # url='https://payment.zarinpal.com/pg/v4/payment/verify.json',
             url='https://sandbox.zarinpal.com/pg/v4/payment/verify.json',
             data=json.dumps(request_data),
             headers=request_header,
         )
-        print(res.text)
+        
         if 'data' in res.json() and ('errors' not in res.json()['data'] or len(res.json()['data']['errors']) == 0):
             data = res.json()['data']
-            print(data)
             payment_code = data['code']
 
             if payment_code == 100:
@@ -174,17 +84,21 @@ def payment_callback(request):
                 order.ref_id = data['ref_id']
                 order.zarinpal_data = data
                 order.save()
-
-                return HttpResponse('پرداخت شما با موفقیت انجام شد.')
-
+                messages.success(request, _('Your payment was successfully registered.'))
+                return redirect(reverse('product_list'))
+            
             elif payment_code == 101:
-                return HttpResponse('پرداخت شما با موفقیت ثبت شد. البته این تراکنش قبلا ثبت شده است.')
-
+                messages.success(request, _('Your payment was successful. However, this transaction has already been recorded.'))
+                return redirect(reverse('product_list'))
+            
             else:
                 error_code = res.json()['errors']['code']
                 error_message = res.json()['errors']['message']
-                return HttpResponse(f'تراکنش نا موفق بود{error_code} {error_message}.')
-
+                messages.warning(request, _(f'The transaction was unsuccessful. {error_code} {error_message}'))
+                return redirect(reverse('product_list'))
+        else:
+            messages.warning(request, _('The transaction was unsuccessful.'))
+            return redirect(reverse('product_list'))
     else:
-        return HttpResponse(f'تراکنش نا موفق بود.')
-
+        messages.warning(request, _('The transaction was unsuccessful.'))
+        return redirect(reverse('product_list')) 
