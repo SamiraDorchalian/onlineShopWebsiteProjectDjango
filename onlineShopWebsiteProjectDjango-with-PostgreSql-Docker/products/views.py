1408ceb6fa33aec4dd2fbb3047bee48c03e8bceb
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-26 17:40:15 +0330

    change template product_list, pagination part

--- onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/products/views.py
+++ onlineShopWebsiteProjectDjango-with-PostgreSql-Docker/products/views.py
@@ -13,7 +13,7 @@ class ProductListView(generic.ListView):
     queryset = Product.objects.filter(active=True)
     template_name = 'products/product_list.html'
     context_object_name = 'products'
-    paginate_by = 3
+    paginate_by = 8
 
 
 class ProductDetailView(generic.DetailView):
