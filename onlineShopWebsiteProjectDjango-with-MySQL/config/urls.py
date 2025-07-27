7bcb9ea666854d54d4b4c28c7d08f855cfc80d5d
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-27 16:14:44 +0330

    create ProductListView

--- onlineShopWebsiteProjectDjango-with-MySQL/config/urls.py
+++ onlineShopWebsiteProjectDjango-with-MySQL/config/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('products/', include('products.urls')),
     # path('accounts/', include('django.contrib.auth.urls')),
     # path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
