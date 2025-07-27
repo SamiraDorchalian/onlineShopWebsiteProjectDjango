21792c789e26bd4ef0e0f798599131e2ee3dd757
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-27 16:27:11 +0330

    create ProductDetailView

--- onlineShopWebsiteProjectDjango-with-MySQL/products/views.py
+++ onlineShopWebsiteProjectDjango-with-MySQL/products/views.py
@@ -9,4 +9,9 @@ class ProductListView(generic.ListView):
     template_name = 'products/product_list.html'
     context_object_name = 'products'
 
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
     
