6f34010a4eba64c9e89715f92dd9310273f49535
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-27 19:02:05 +0330

    add Comment Model

--- onlineShopWebsiteProjectDjango-with-MySQL/products/admin.py
+++ onlineShopWebsiteProjectDjango-with-MySQL/products/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'price', 'active', 'datetime_create', 'datetime_modified']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['product', 'author', 'body', 'stars', 'datetime_create', 'datetime_modified', 'active']
--- onlineShopWebsiteProjectDjango-with-MySQL/products/models.py
+++ onlineShopWebsiteProjectDjango-with-MySQL/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -17,4 +18,22 @@ class Product(models.Model):
         return reverse("product_detail", args=[self.pk])
     
 
-    
+class Comment(models.Model):
+    PRODUCT_STARS = [
+        ('1', '🙄'),
+        ('2', '🤔🤔'),
+        ('3', '😪😪😪'),
+        ('4', '😊😊😊😊'),
+        ('5', '😍😍😍😍😍'),
+        ('6', '🤩🤩🤩🤩🤩🤩'),
+    ]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', )
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments',)
+    body = models.TextField()
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+
+    datetime_create = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    active = models.BooleanField(default=True)
+
