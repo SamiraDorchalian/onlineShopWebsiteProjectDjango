095b9dcb879cfc693a944af3eec446304a5025a5
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-28 13:35:31 +0330

    add TabularInline for ProductComments

--- onlineShopWebsiteProjectDjango-with-MySQL/products/admin.py
+++ onlineShopWebsiteProjectDjango-with-MySQL/products/admin.py
@@ -2,9 +2,20 @@ from django.contrib import admin
 
 from .models import Product, Comment
 
+
+class ProductCommentsInLine(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active']
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'price', 'active', 'datetime_create', 'datetime_modified']
+
+    inlines = [
+        ProductCommentsInLine,
+    ]
 
 
 @admin.register(Comment)
