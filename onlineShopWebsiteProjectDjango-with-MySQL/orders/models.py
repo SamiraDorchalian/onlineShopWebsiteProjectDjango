528ed26646df76a20e347ceb1185430a32f6f159
samira dorchalian <samiradorchalain1998@gmail.com>
2025-07-29 18:31:53 +0330

    add orderItem Model

--- onlineShopWebsiteProjectDjango-with-MySQL/orders/models.py
+++ onlineShopWebsiteProjectDjango-with-MySQL/orders/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext as _
 
+from products.models import Product
+
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_('User'))
-    is_paid = models.BooleanField(default=False verbose_name=_('Is Paid'))
+    is_paid = models.BooleanField(default=False, verbose_name=_('Is Paid'))
 
     first_name = models.CharField(max_length=100, verbose_name=_('First Name'))
     last_name = models.CharField(max_length=100, verbose_name=_('Last Name'))
@@ -15,4 +17,15 @@ class Order(models.Model):
     datetime_create = models.DateTimeField(auto_now_add=True, verbose_name=_('DateTimeCreate'))
     datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('DateTimeModified'))
 
+    def __str__(self):
+        return f'Order {self.id}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'OrderItem {self.id}: {self.product} x {self.quantity} (price:{self.price})'
     
